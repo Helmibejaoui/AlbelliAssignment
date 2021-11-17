5899772c0c525f441728c4e8b803ebafa16cceb2
helmi.bejaoui <helmi.bejaoui@mazars.fr>
2021-11-17 12:23:09 +0100

    fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Advertisement;
+use DateInterval;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Exception;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @throws Exception
+     */
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 20; ++$i) {
+            $currentDate = new DateTime('NOW');
+            $advertisement = new Advertisement();
+            $advertisement->setTitle('advertisement '.$i);
+            $advertisement->setValidUntil($currentDate->add(new DateInterval('P'.($i + 1).'D')));
+            $advertisement->setLink('https://test.com');
+            $manager->persist($advertisement);
+        }
+
+        $manager->flush();
+    }
+}

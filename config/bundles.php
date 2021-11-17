e02e23e7146ed11451a017414b1b8243e149c13f
helmi.bejaoui <helmi.bejaoui@mazars.fr>
2021-11-17 21:29:54 +0100

    config

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,15 @@
 <?php
 
+/*
+ * @owner        : 20 mars street,Mateur, Bizerte, Tunisia
+ * @contact      : bejaoui.helmi@gmail.com
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * @owner        : 20 mars street,Mateur, Bizerte, Tunisia
+ * @contact      : bejaoui.helmi@gmail.com
+ */
+
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
